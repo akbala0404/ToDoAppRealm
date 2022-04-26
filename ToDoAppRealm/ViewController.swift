6834c2f400e2b9875a007d3886183976d6e763ca
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2022-04-27 00:19:25 +0600

    Initial Commit

---
+++ ToDoAppRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoAppRealm
+//
+//  Created by Акбала Тлеугалиева on 4/27/22.
+//  Copyright © 2022 Akbala Tleugaliyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
